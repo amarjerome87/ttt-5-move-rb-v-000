bd976af1d60cb53291368ab1bd7bcaf26b4da684
Amar Chatman <amarjerome@gmail.com>
2017-10-25 03:16:12 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -8,3 +8,8 @@ end
 def input_to_index(input)
   input.to_i + 1
 end
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+def method (array,index,player = "X")
+  array[index] = player 
+end 
+  
